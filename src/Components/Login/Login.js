8ca8f8b92077b89d439b3ea6e137029122f96b7c
RenatoReboucas <renato190698@gmail.com>
2020-09-18 23:57:58 -0300

    terminando estilo da tela de login

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -4,20 +4,25 @@ import LoginForm from './LoginForm'
 import LoginCreate from "./LoginCreate";
 import LoginPasswordLost from "./LoginCreate";
 import LoginPasswordReset from "./LoginPasswordReset";
+
+import styles from './Login.module.css'
+
 import { UserContext } from '../../UserContext';
 
 function Login() {
   const {login} = React.useContext(UserContext)
   if( login === true ) return <Navigate to="/conta" />
   return (
-    <div>
-      <Routes>
-        <Route path="/" element={<LoginForm />} />
-        <Route path="criar" element={<LoginCreate />} />
-        <Route path="perdeu" element={<LoginPasswordLost />} />
-        <Route path="resetar" element={<LoginPasswordReset />} />
-      </Routes>
-    </div>
+    <section className={styles.login}>
+      <div className={styles.forms}>
+        <Routes>
+          <Route path="/" element={<LoginForm />} />
+          <Route path="criar" element={<LoginCreate />} />
+          <Route path="perdeu" element={<LoginPasswordLost />} />
+          <Route path="resetar" element={<LoginPasswordReset />} />
+        </Routes>
+      </div>
+    </section>
   );
 }
 
