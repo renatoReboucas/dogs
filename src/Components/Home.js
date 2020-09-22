3c4d6e66d4a0a84e8cfd92f9996c485e9fa7c98e
RenatoReboucas <renato190698@gmail.com>
2020-09-22 13:51:54 -0300

    criando parte inicial do feed

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import Feed from './Feed/Feed'
 
 const Home = () => {
   return (
-    <div>
-      <h1>HOME</h1>
-    </div>
+    <section className="container mainContainer">
+      <Feed />
+    </section>
   )
 }
 
